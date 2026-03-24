3a7e9dc74b3d6294e95d0482d97b1a2e274be04e
Op1m <commercialop1m@gmail.com>
2026-03-24 18:45:14 +0700

    dad

--- backend/src/main/java/com/op1m/medrem/backend_api/config/SecurityConfig.java
+++ backend/src/main/java/com/op1m/medrem/backend_api/config/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -23,11 +21,6 @@ public class SecurityConfig {
 
     @Autowired
     private UserService userService;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
